bd9548594cb1668ad84f36c05c7a56311c95bd3d
KaRimElhlag <kareemm.elhlag55@gmail.com>
2026-01-19 21:39:57 +0300

    Initial commit - .NET 9 Microservices

---
+++ ReceiverApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory { HostName = "rabbitmq" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "orders", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [v] Received: {message}");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queue: "orders", autoAck: true, consumer: consumer);
+Console.WriteLine(" [*] Waiting for messages. To exit press CTRL+C");
+await Task.Delay(-1); 
---
+++ SenderApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "rabbitmq" }; 
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "orders", durable: false, exclusive: false, autoDelete: false, arguments: null);
+
+while (true)
+{
+    string message = $"New Order at {DateTime.Now.ToLongTimeString()}";
+    var body = Encoding.UTF8.GetBytes(message);
+    await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "orders", body: body);
+    Console.WriteLine($" [x] Sent '{message}'");
+    await Task.Delay(2000); 
+}
